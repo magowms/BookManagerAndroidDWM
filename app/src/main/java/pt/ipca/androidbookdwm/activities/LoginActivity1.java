d090ccc3de34d32bebd6934c9f62143f27475358
paulo <magowms@gmail.com>
2019-11-18 20:14:14 +0000

    App Form

--- app/src/main/java/pt/ipca/androidbookdwm/activities/LoginActivity1.java
+++ app/src/main/java/pt/ipca/androidbookdwm/activities/LoginActivity1.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import pt.ipca.androidbookdwm.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login1);
     }
 }
