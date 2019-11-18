2be0dbb4347179a47ab63f3d83f7abd9a8e105c6
paulo <magowms@gmail.com>
2019-11-18 20:05:45 +0000

    1

---
+++ app/src/main/java/pt/ipca/androidbookdwm/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package pt.ipca.androidbookdwm.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipca.androidbookdwm.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
